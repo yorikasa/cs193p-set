84aea1b0f23abbd9562e306df6ef82d196b594e7
Yuki Orikasa <yuki.wter@gmail.com>
2018-02-11 17:30:05 +0900

    Add game initialization

--- Set/Set.swift
+++ Set/Set.swift
@@ -10,13 +10,29 @@ import Foundation
 
 class Set {
     //    MARK: - Variables
+    
+    // card variations
     let figures = ["●", "▲", "■"]
+    let number = 1...3
+    let shading = ["solid", "striped", "open"]
+    
+    // game rules
+    let initialVisibleCards: Int = 12
+    var cardsSelecting: [Card] = []
     
     var cards: [Card]
     
     //    MARK: - Methods
     
     init() {
+        // Initialize all 81 cards
         cards = []
+        for f in figures {
+            for n in number {
+                for s in shading {
+                    cards.append(Card(figure: f, number: n, shading: s))
+                }
+            }
+        }
     }
 }
