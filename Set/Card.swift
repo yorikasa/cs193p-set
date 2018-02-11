ae29935ce833bf4588b3465689e4888c144600a9
Yuki Orikasa <yuki.wter@gmail.com>
2018-02-11 17:06:04 +0900

    Implement Card struct

--- Set/Card.swift
+++ Set/Card.swift
@@ -9,5 +9,24 @@
 import Foundation
 
 struct Card {
+    let id: Int
     
+    let figure: String
+    let number: Int
+    let shading: String
+    
+    static var idGenerator = 0
+    
+    static func createId() -> Int {
+        self.idGenerator += 1
+        return self.idGenerator
+    }
+    
+    init(figure: String, number: Int, shading: String){
+        self.id = Card.createId()
+        
+        self.figure = figure
+        self.number = number
+        self.shading = shading
+    }
 }
