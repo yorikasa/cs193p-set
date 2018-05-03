75cded0dab7c2d382f651a29a09e44729f39f0cc
Yuki Orikasa <yuki.wter@gmail.com>
2018-05-03 23:31:38 +0900

    Make use of Card.matches(to: and:)

--- Set/Set.swift
+++ Set/Set.swift
@@ -105,7 +105,7 @@ class Set {
     
     func doesFormSet(with cards: [Card]) -> Bool {
         // for the test purpose
-        return true
+        //return true
         
         // If all attributes of 3 cards are either different or same, they make a set
         // e.g. figures of all cards are same (●)
@@ -113,32 +113,7 @@ class Set {
         //      shading of all cards are different (solid, striped, and open)
         //      color   of all cards are same (blue)
         // these cards form a set!
-        var figures: [Int] = []
-        var numbers: [Int] = []
-        var shading: [Int] = []
-        var colors: [Int] = []
-        
-        for card in cards {
-            figures.append(card.figureId)
-            numbers.append(card.numberId)
-            shading.append(card.shadingId)
-            colors.append(card.colorId)
-        }
-        
-        if !(figures.hasAllSameElements || figures.hasAllDifferentElements) {
-            return false
-        }
-        if !(numbers.hasAllSameElements || numbers.hasAllDifferentElements) {
-            return false
-        }
-        if !(shading.hasAllSameElements || shading.hasAllDifferentElements) {
-            return false
-        }
-        if !(colors.hasAllSameElements || colors.hasAllDifferentElements) {
-            return false
-        }
-        
-        return true
+        return cards[0].matches(to: cards[1], and: cards[2])
     }
     
 
