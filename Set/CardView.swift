52e3001bea47745ae18f43c2631f3cd1ce15c64e
Yuki Orikasa <yuki.wter@gmail.com>
2018-05-05 14:18:51 +0900

    Add custom view CardView

---
+++ Set/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  Set
+//
+//  Created by Yuki Orikasa on 2018/05/05.
+//  Copyright © 2018 Yuki Orikasa. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
