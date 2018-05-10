56f42e4da3e5886899a94a048e143307bec40186
Yuki Orikasa <yuki.wter@gmail.com>
2018-05-11 00:06:41 +0900

    Fix card view updates after card replaces

--- Set/Controllers/ViewController.swift
+++ Set/Controllers/ViewController.swift
@@ -46,7 +46,49 @@ class ViewController: UIViewController {
     
     // MARK: - Functions
     func updateCardsView() {
-        assert(cardViews.count == game.openCards.count, "model != view")
+        assert(cardViews.count == game.openCards.count, "model \(cardViews.count) != view \(game.openCards.count)")
+        let cardViewIds = cardViews.map({$0.id})
+        let cardIds = game.openCards.map({$0.id})
+        let diff = idDifference(cardIds: cardIds, cardViewIds: cardViewIds)
+
+        for i in 0..<diff.missing.count {
+            // the cards were removed from model because of matching etc
+            // so i have to remove them from views too
+            var cardRect = CGRect(origin: CGPoint.zero, size: CGSize.zero)
+            if let index = cardViews.index(where: {$0.id == diff.missing[i]}) {
+                cardRect = cardViews[index].frame
+                cardViews[index].removeFromSuperview()
+                cardViews.remove(at: index)
+                
+                if let cardIndex = game.openCards.index(where: {$0.id == diff.added[i]}) {
+                    openCardView(of: game.openCards[cardIndex], with: cardRect, at: index)
+                }
+            }
+            
+        }
+    }
+    
+    private func printCardViews() {
+        var strings = [String]()
+        for cardView in cardViews {
+            var string: String = ""
+            switch cardView.figure {
+            case Card.Figure.circle:
+                for _ in 0..<cardView.numbers.rawValue+1 {
+                    string.append("◯")
+                }
+            case Card.Figure.square:
+                for _ in 0..<cardView.numbers.rawValue+1 {
+                        string.append("⬜︎")
+                }
+            case Card.Figure.triangle:
+                for _ in 0..<cardView.numbers.rawValue+1 {
+                    string.append("△")
+                }
+            }
+            strings.append(string)
+        }
+        print(strings)
     }
     
     private func removeCardViews() {
@@ -96,16 +138,16 @@ extension ViewController {
                 cardViewsGrid.cellCount += 1
                 if let lastCellRect = cardViewsGrid[cardViewsGrid.cellCount - 1] {
                     let cardRect = CGRect(origin: cardOrigin(origin: lastCellRect.origin, size: lastCellRect.size), size: cardSize(from: lastCellRect.size))
-                    openCardView(of: card, at: cardRect)
+                    openCardView(of: card, with: cardRect)
                 }
             }
         }
         rearrangeCardViews()
     }
     
-    private func openCardView(of card: Card, at cardRect: CGRect) {
+    private func openCardView(of card: Card, with cardRect: CGRect, at index: Int? = nil) {
         let cardView = CardView(frame: cardRect)
-        cardViews.append(cardView)
+        cardViews.insert(cardView, at: index ?? cardViews.endIndex)
         cardsMatView.addSubview(cardView)
         cardView.id = card.id
         cardView.setAttributes(figure: Card.Figure(rawValue: card.figureId)!,
@@ -131,6 +173,20 @@ extension ViewController {
     }
     private func cardSize(from size: CGSize) -> CGSize {
         return size.shrink(to: Constant.cardShrinkRatio)
+    }
+    
+    private func idDifference(cardIds: [Int], cardViewIds: [Int]) -> (added: [Int], missing: [Int]) {
+        var cardIdAdded =  cardIds
+        var cardViewIdMissing = cardViewIds
+        for cardIdIndex in cardIdAdded.indices {
+            if let cardViewIdIndex = cardViewIds.index(where: {$0 == cardIds[cardIdIndex]}) {
+                cardIdAdded[cardIdIndex] = 0
+                cardViewIdMissing[cardViewIdIndex] = 0
+            }
+        }
+        let added = cardIdAdded.filter({$0 != 0})
+        let missing = cardViewIdMissing.filter({$0 != 0})
+        return (added: added, missing: missing)
     }
 }
 
@@ -163,7 +219,7 @@ extension ViewController {
             if let cardView = sender.view as? CardView {
                 game.selectCard(at: cardView.id)
                 cardView.isSelected = !cardView.isSelected
-                // TODO: implement (maybe i should use updateCardsView())
+                updateCardsView()
             }
         default:
             break
